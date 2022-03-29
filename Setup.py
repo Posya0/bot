75d4975d2a0689002d27043b0ae7703843fd0778
nastyapodv <podvoiskaia_nastya@mail.ru>
2022-03-29 16:10:20 +0300

    Fix bugs

--- Setup.py
+++ Setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
 
-def get_dependencies(): #достаем зависимости из файла requirements.txt
-    with open("bot/requirements.txt", "r") as f:
-        return f.read().split("\n")
-
-
 setup(
     name='bot',
     version='1.0.0',
     packages=['bot'],
-    install_requires=get_dependencies(),
+    install_requires=[
+        "requests~=2.25.1",
+        "bs4~=0.0.1",
+        "beautifulsoup4~=4.9.3",
+        "Pillow~=8.2.0",
+        "json~=2.5.0",
+        "vk_api~=11.9.3"
+    ],
     package_data={
         'bot': ['znaki_pictures/*']
     }
