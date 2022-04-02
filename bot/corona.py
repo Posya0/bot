a6fdb7d5636411c10356a13b190be44a3960ff3b
Posya0 <posya.prozorova.02@gmail.com>
2022-04-03 00:07:42 +0300

    статистика короновируса по России. исправленно

--- bot/corona.py
+++ bot/corona.py
@@ -1,6 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
+page = requests.get("https://coronavirusstat.ru")
+soup = BeautifulSoup(page.text, "html.parser")
+way = soup.find("div", {"class": "row justify-content-md-center"}).find_all("div",
+                                                                            {"class": "col col-6 col-md-3 pt-4"})
+data_sost = soup.find("h6", {"class": "text-muted"}).find("strong")
+
 def get_new(region, ans):
     """ в строку добавляются новые данные со статистикой по региону """
     s = region.text.split()
@@ -9,9 +15,6 @@ def get_new(region, ans):
 
 def in_regions(reg):
     """статистика короны в регионах """
-    page = requests.get("https://coronavirusstat.ru")
-    soup = BeautifulSoup(page.text, "html.parser")
-    data_sost = soup.find("h6", {"class": "text-muted"}).find("strong")
     in_region = soup.find_all("div", {"class": "row border border-bottom-0 c_search_row"})
     ans = "По состоянию на " + data_sost.text + ":\n"
     path = soup
@@ -35,15 +38,10 @@ def in_regions(reg):
 
 def in_russia():
     """статистика по коронавирусу в России"""
-    page = requests.get("https://coronavirusstat.ru")
-    soup = BeautifulSoup(page.text, "html.parser")
-    way = soup.find("div", {"class": "row justify-content-md-center"}).find_all("div",
-                                                                                {"class": "col col-6 col-md-3 pt-4"})
-    data_sost = soup.find("h6", {"class": "text-muted"}).find("strong")
     ans = "По состоянию на " + data_sost.text + ":\n"
     for x in way:
         s = x.text.split()
         ans += s[6] + ": " + s[4] + " " + s[5] + "(" + s[0] + " сегодня)\n"
     return ans
 
-print(in_russia())
+
