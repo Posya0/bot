1a17d286f18eb5a16b95621b03c0884b46d91d68
Posya0 <posya.prozorova.02@gmail.com>
2022-04-20 14:46:44 +0300

    короновирус. закончен

--- bot/corona.py
+++ bot/corona.py
@@ -1,4 +1,6 @@
 import datetime
+import re
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -10,11 +12,91 @@ way = soup.find("div", {"class": "row justify-content-md-center"}).find_all("div
                                                                             {"class": "col col-6 col-md-3 pt-4"})
 data_sost = soup.find("h6", {"class": "text-muted"}).find("strong")
 
+"""сайт блокирует доступ(ошибка 403),
+    поэтому мы "притворимся" браузером """
+
+url_world = 'https://index.minfin.com.ua/reference/coronavirus/'
+header = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
+        "X-Requested-With": "XMLHttpRequest"
+    }
+
 def get_new(region, ans):
     """ в строку добавляются новые данные со статистикой по региону """
     s = region.text.split()
     ans += s[0] + ": " + s[1] + "\n"
     return ans
+
+
+def grafic(url_end):
+    """ строим график"""
+    """получаем ссылку"""
+
+    url = url_world + url_end + (str(datetime.date.today()))[:-3] + '/'
+    print(url)
+    r = requests.get(url, headers=header)
+
+    """достаем таблицу с сайта, скрываем 
+    последний столбец и переименовываем столбцы"""
+
+    if url_end == '':
+        flag = 0
+    else:
+        flag = 1
+
+    df = (pd.read_html(r.text))[flag]
+    df.rename(columns={'Всегозара­жений': 'Всего случаев', 'Смер­тельныеслучаи': 'Умерло',
+                       'Выздоро­вевшие':'Вылечено','Боле­ющие':'Активных'}, inplace=True)
+
+    """делаем график"""
+
+    fig = df.plot(x="Дата", y=["Всего случаев", "Умерло", "Вылечено", "Активных"])
+    fig.set_ylabel('кол-во, [млн] ')
+    plt.title('Статистика за месяц', fontsize=10)
+
+    """сохраняем график"""
+
+    pic = fig.figure
+    pic.savefig('files/covid.png')
+
+
+def find_region(city):
+    """определить регион по городу"""
+    """достаем таблицу, забираем из нее 2 колонки:
+     город и область"""
+
+    df = (pd.read_html('https://hramy.ru/regions/city_abc.htm'))[0]
+    df.drop(("Район/подчинение"), axis=1, inplace=True)
+    df.drop(("Код"), axis=1, inplace=True)
+    df.drop(("Центр"), axis=1, inplace=True)
+
+    """ находим область по городу"""
+
+    city = city.title()
+
+    region = ''
+    for i in range(df.shape[0]):
+        # print(df.iat[i, 0])
+        if df.iat[i, 0] == city:
+            region = df.iat[i, 1]
+    res = (re.findall(r'^\w+', region))[0]
+
+    return res
+
+
+
+def is_city(city):
+    """Проверяем, существует ли город"""
+    df = (pd.read_html('https://hramy.ru/regions/city_abc.htm'))[0]
+    df.drop(("Район/подчинение"), axis=1, inplace=True)
+    df.drop(("Код"), axis=1, inplace=True)
+    df.drop(("Центр"), axis=1, inplace=True)
+
+    for i in range(df.shape[0]):
+        if city.lower() in (df.iat[i, 0]).lower():
+            return True
+    return False
+
 
 def in_regions(reg):
     """статистика короны в регионах """
@@ -39,22 +121,19 @@ def in_regions(reg):
     ans = get_new(path, ans)
     return ans
 
+
 def in_russia():
     """статистика по коронавирусу в России"""
     ans = "По состоянию на " + data_sost.text + ":\n"
     for x in way:
         s = x.text.split()
         ans += s[6] + ": " + s[4] + " " + s[5] + "(" + s[0] + " сегодня)\n"
+    grafic('geography/russia/')
     return ans
 
 
 def in_world():
     """статистика по коронавирус в мире"""
-    url_world = 'https://index.minfin.com.ua/reference/coronavirus/'
-    header = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
-        "X-Requested-With": "XMLHttpRequest"
-    }
     r = requests.get(url_world, headers=header)
     df = (pd.read_html(r.text))[0]
     ans = "По состоянию на " + data_sost.text + ":\n"
@@ -62,21 +141,6 @@ def in_world():
     ans += "Умерло: " + df.iat[1, 1] + "\n"
     ans += "Вылечено: " + df.iat[0, 1] + "\n"
     ans += "Активные: " + df.iat[0, 1]
-
-    url = url_world + (str(datetime.date.today()))[:-3] + '/'
-    print(url)
-    r = requests.get(url, headers=header)
-
-    df = (pd.read_html(r.text))[0]
-    df.rename(columns={'Всегозара­жений': 'Всего случаев', 'Смер­тельныеслучаи': 'Умерло',
-                       'Выздоро­вевшие': 'Вылечено', 'Боле­ющие': 'Активных'}, inplace=True)
-
-    fig = df.plot(x="Дата", y=["Всего случаев", "Умерло", "Вылечено", "Активных"])
-    fig.set_ylabel('кол-во, [млн] ')
-    plt.title('Статистика за месяц', fontsize=10)
-
-    pic = fig.figure
-    pic.savefig('images/covid.png')
+    grafic('')
     return ans
 
-print(in_world())
